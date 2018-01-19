b866f5945d1b78b3bb0f17d5342ecc818f27fd3f
Atsushi-Koyama <two51ct13th@yahoo.co.jp>
2018-01-20 08:31:34 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy, :delete, :edit, :update]
   resources :prototypes, only: [:index, :new, :create, :show]
   resources :users, only: [:show, :edit, :update]
 
